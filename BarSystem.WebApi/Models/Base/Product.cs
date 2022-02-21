910dd7022f6970d3074a2289059796df73f65628
Patricio <pato.rocaspana@gmail.com>
2022-02-21 18:08:18 -0300

    Improve Models and delete unused classes

--- BarSystem.WebApi/Models/Base/Product.cs
+++ BarSystem.WebApi/Models/Base/Product.cs
@@ -1,9 +1,12 @@
-﻿namespace BarSystem.WebApi.Models.Base
+﻿using BarSystem.Models.Base;
+
+namespace BarSystem.WebApi.Models.Base
 {
-    public abstract class Product
+    public abstract class Product : Entity
     {
         public string Name { get; set; }
         public string Description { get; set; }
         public float Price { get; set; }
+        public int Stock { get; set; }
     }
 }
--- BarSystem.WebApi/Models/Table.cs
+++ BarSystem.WebApi/Models/Table.cs
@@ -7,8 +7,16 @@ namespace BarSystem.Models
     {
         public Employee Waiter { get; set; }
         public int AmountPeople { get; set; }
+        public bool ExistAdult { get; set; }
         public List<Drink> Drinks { get; set; } = new List<Drink>();
         public List<Dish> Dishes { get; set; } = new List<Dish>();
-        public float TotalPrice { get; set; }
+        public float TotalPrice => GetTotalPrice();
+
+        private float GetTotalPrice()
+        {
+            var totalPrice = Drinks.Sum(drink => drink.Price);
+            totalPrice += Dishes.Sum(dish => dish.Price);
+            return totalPrice;
+        }
     }
 }

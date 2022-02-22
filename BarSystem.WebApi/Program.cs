fd3fe53e8947689ec4437874f9c0b0b6af3af465
Patricio <pato.rocaspana@gmail.com>
2022-02-22 12:58:24 -0300

    Fix ConnectionStrings param

--- BarSystem.WebApi/Program.cs
+++ BarSystem.WebApi/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<BarSystemDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("name=ConnectionStrings:Database"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Database"));
 });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

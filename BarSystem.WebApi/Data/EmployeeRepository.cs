a7d1310e1f2332c9282dad4846025975aa163c99
Patricio <pato.rocaspana@gmail.com>
2022-02-22 18:33:55 -0300

    Create Repositories

---
+++ BarSystem.WebApi/Data/EmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using BarSystem.WebApi.Interfaces.Data;
+using BarSystem.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarSystem.WebApi.Data
+{
+    public class EmployeeRepository : BaseRepository<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(BarSystemDbContext dbContext) : base(dbContext) { }
+
+        protected override void UpdateEntity(Employee existingEmployee, Employee newEmployee)
+        {
+            existingEmployee.Dni = newEmployee.Dni;
+            existingEmployee.FirstName = newEmployee.FirstName;
+            existingEmployee.LastName = newEmployee.LastName;
+            existingEmployee.Role = newEmployee.Role;
+        }
+    }
+}

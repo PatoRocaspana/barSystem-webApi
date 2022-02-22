a7edc9b84a308405a8c03571839c10dce6818b85
Patricio <pato.rocaspana@gmail.com>
2022-02-22 18:34:23 -0300

    Create Interfaces of Repositories

---
+++ BarSystem.WebApi/Interfaces/Data/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using BarSystem.WebApi.Models.Base;
+
+namespace BarSystem.WebApi.Interfaces.Data
+{
+    public interface IBaseRepository<T> where T : Entity
+    {
+        Task<T> CreateAsync(T entity);
+        Task DeleteAsync(int id);
+        Task<bool> EntityExistsAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<T> GetAsync(int id);
+        Task<T> UpdateAsync(T entity, int id);
+    }
+}
---
+++ BarSystem.WebApi/Interfaces/Data/IDishRepository.cs
@@ -0,0 +1,6 @@
+﻿using BarSystem.WebApi.Models;
+
+namespace BarSystem.WebApi.Interfaces.Data
+{
+    public interface IDishRepository : IBaseRepository<Dish> { }
+}

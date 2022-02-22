dd7e847079966f52d6252f7e7558992e216c4b8c
Patricio <pato.rocaspana@gmail.com>
2022-02-22 11:45:56 -0300

    Create and configure DbContext

---
+++ BarSystem.WebApi/Data/BarSystemDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BarSystem.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarSystem.WebApi.Data
+{
+    public class BarSystemDbContext : DbContext
+    {
+        public BarSystemDbContext(DbContextOptions<BarSystemDbContext> options) : base(options) { }
+
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Drink> Drinks { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Table> Tables { get; set; }
+    }
+}
--- BarSystem.WebApi/Program.cs
+++ BarSystem.WebApi/Program.cs
@@ -1,8 +1,16 @@
+using BarSystem.WebApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<BarSystemDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("name=ConnectionStrings:Database"));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

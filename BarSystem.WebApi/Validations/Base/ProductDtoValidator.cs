4afa635199469bbae3a2fcebb7772a46dc9320e7
Patricio <pato.rocaspana@gmail.com>
2022-02-23 20:54:39 -0300

    Add Product and Dish validators

---
+++ BarSystem.WebApi/Validations/Base/ProductDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using BarSystem.WebApi.DTOs.Base;
+using FluentValidation;
+
+namespace BarSystem.WebApi.Validations.Base
+{
+    public class ProductDtoValidator : AbstractValidator<ProductDto>
+    {
+        public ProductDtoValidator()
+        {
+            RuleFor(prop => prop.Name)
+                                    .Cascade(CascadeMode.Stop)
+                                    .NotEmpty()
+                                    .Length(2, 30);
+
+            RuleFor(prop => prop.Description)
+                                    .MaximumLength(200);
+
+            RuleFor(prop => prop.Price)
+                                    .GreaterThan(0);
+
+            RuleFor(prop => prop.Stock)
+                                    .GreaterThanOrEqualTo(0);
+        }
+    }
+}
---
+++ BarSystem.WebApi/Validations/DishDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using BarSystem.WebApi.DTOs;
+using BarSystem.WebApi.Validations.Base;
+using FluentValidation;
+
+namespace BarSystem.WebApi.Validations
+{
+    public class DishDtoValidator : AbstractValidator<DishDto>
+    {
+        public DishDtoValidator()
+        {
+            Include(new ProductDtoValidator());
+
+            RuleFor(prop => prop.EstimatedTime)
+                                    .InclusiveBetween(TimeSpan.Zero, TimeSpan.FromMinutes(150)).WithMessage("{PropertyName} must be between 0 and 150 minutes");
+
+            RuleFor(prop => prop.Category)
+                                    .IsInEnum();
+        }
+    }
+}
